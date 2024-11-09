47d3cb00d89a55084ce8b0af3e636588d534c49f
Suresh Baskar <38977541+vagaisuresh@users.noreply.github.com>
2024-11-09 17:46:16 +0530

    Create a navbar in JSX

--- react-app/index.js
+++ react-app/index.js
@@ -1,9 +1,41 @@
 //ReactDOM.render(<h1>Hello, Suresh!</h1>, document.getElementById("root"))
 
-ReactDOM.render(
+/* ReactDOM.render(
     <ul>
         <li>First List</li>
         <li>Second List</li>
     </ul>,
     document.getElementById("root")
+) */
+
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+/* 
+Challenge: 
+
+Create a navbar in JSX:
+    - Use the semantic `nav` element as the parent wrapper
+    - Have an h1 element with the brand name of your "website"
+    - Insert an unordered list for the other nav elements
+        - Inside the `ul`, have three `li`s for "Pricing",
+        "About", and "Contact"
+    - Don't worry about styling yet - it'll just be plain-looking HTML for now
+*/
+
+const page = (
+    <nav>
+        <h1>My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
 )
+
+ReactDOM.render(page, document.getElementById("root"))
