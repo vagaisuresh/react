a74fa6a77451d3ccf927725bc36c34f37381f26d
Suresh Baskar <38977541+vagaisuresh@users.noreply.github.com>
2024-11-09 17:26:42 +0530

    First React app using CDN

---
+++ react-app/index.js
@@ -0,0 +1,9 @@
+//ReactDOM.render(<h1>Hello, Suresh!</h1>, document.getElementById("root"))
+
+ReactDOM.render(
+    <ul>
+        <li>First List</li>
+        <li>Second List</li>
+    </ul>,
+    document.getElementById("root")
+)
